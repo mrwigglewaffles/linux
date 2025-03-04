ec5da8041ed49a0427b1f379a928d783591a5e5b
Fiona Behrens <me@kloenk.dev>
2025-03-04 18:52:05 +0100

    rust: io: add PortIo
    Add `rust::io::PortIo` implementing the `IoAccess` trait.
    
    Signed-off-by: Fiona Behrens <me@kloenk.dev>
    Signed-off-by: Andrew Ballance <andrewjballance@gmail.com>

--- rust/helpers/io.c
+++ rust/helpers/io.c
@@ -51,3 +51,23 @@ define_rust_mmio_write_helper(writel_relaxed, u32);
 #ifdef CONFIG_64BIT
 define_rust_mmio_write_helper(writeq_relaxed, u64);
 #endif
+
+#define define_rust_pio_read_helper(name, type)     \
+	type rust_helper_##name(unsigned long port) \
+	{                                           \
+		return name(port);                  \
+	}
+
+#define define_rust_pio_write_helper(name, type)                \
+	void rust_helper_##name(type value, unsigned long port) \
+	{                                                       \
+		name(value, port);                              \
+	}
+
+define_rust_pio_read_helper(inb, u8);
+define_rust_pio_read_helper(inw, u16);
+define_rust_pio_read_helper(inl, u32);
+
+define_rust_pio_write_helper(outb, u8);
+define_rust_pio_write_helper(outw, u16);
+define_rust_pio_write_helper(outl, u32);
